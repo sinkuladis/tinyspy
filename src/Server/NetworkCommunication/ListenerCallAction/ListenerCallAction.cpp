dfa4c7ab05f426b89250b2132c6821d91d392b26
usinkevi <sink.vlad@gmail.com>
2019-04-14 21:40:11 +0200

    Removed version control marker

--- src/Server/NetworkCommunication/ListenerCallAction/ListenerCallAction.cpp
+++ src/Server/NetworkCommunication/ListenerCallAction/ListenerCallAction.cpp
@@ -4,11 +4,8 @@
 #include <iostream>
 #include <thread>
 #include <unistd.h>
-<<<<<<< HEAD
 #include <cstring>
 #include <future>
-=======
->>>>>>> bbcca62a240760b3b75b39101bddcfedc2ca6888
 
 #define MAX_MSG_LEN 4096
 
@@ -18,7 +15,6 @@ void addNewClient(int clientSocket, std::list<Client> &clients) {
   newClient.mock_answer();
 }
 
-<<<<<<< HEAD
 
 void connectClients(int mainSocket, struct sockaddr_in server, std::list<Client> &clients, std::future<void> futureObj) {
     while (futureObj.wait_for(std::chrono::milliseconds(1)) == std::future_status::timeout) {
@@ -31,15 +27,4 @@ void connectClients(int mainSocket, struct sockaddr_in server, std::list<Client>
         }
     }
     std::cout << "Wylaczanie odbierania polaczen" << std::endl;
-=======
-void connectClients(int mainSocket, struct sockaddr_in server,
-                    std::list<Client> &clients) {
-  while (true) {
-    printf("Waiting for connection...\n");
-    struct sockaddr_in client = {};
-    socklen_t len = sizeof(server);
-    int clientSocket = accept(mainSocket, (struct sockaddr *)&client, &len);
-    addNewClient(clientSocket, clients);
-  }
->>>>>>> bbcca62a240760b3b75b39101bddcfedc2ca6888
 }
