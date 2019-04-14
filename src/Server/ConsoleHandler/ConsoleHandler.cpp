653da66e1c254569442c8adb4ccfb8c63e26e98c
Ewelina <ewelkos.chmielewska@gmail.com>
2019-04-14 18:09:53 +0200

    Czytanie z konsoli z wykorzystaniem getchar() zamiast cin

--- src/Server/ConsoleHandler/ConsoleHandler.cpp
+++ src/Server/ConsoleHandler/ConsoleHandler.cpp
@@ -1,13 +1,40 @@
 #include "ConsoleHandler.hpp"
 #include <iostream>
+#include <cstdio>
+#include <stdio.h>
+#include <string.h>
 
+#define MAX_INPUT 20
+
+
+void runcommand(std::string command) {
+    if("shutdown" == command) {
+        std::cout << "Zamykanie serwera" << std::endl;
+        exit(0);
+    }
+    
+}
 
 void handleConsole() {
-    while(true) {
-        std::string command;
-        std::cin >> command;
+    char buffer[MAX_INPUT];
+    int idx = 0;
 
-        if( command == "shutdown" )
-            exit(1);
+    while(true) {
+        char inputChar = getchar();
+        if( idx <  MAX_INPUT - 1) {
+            buffer[idx] = inputChar;
+            if (inputChar == '\n') {
+                buffer[idx] = '\0';
+                runcommand(buffer);
+                idx = 0;
+            }
+            else
+                idx++;
+        }
+        else {
+            std::cout << "Maksymalna dlugosc komendy wynosi " << MAX_INPUT;
+            std::cout << ". Reset komendy" << std::endl;
+            idx = 0;
+        }
     }
 }
