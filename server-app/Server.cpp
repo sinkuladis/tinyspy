bf56e7117f228645bf096beb77697b436deeb97d
ziemus <musiatowiczz@gmail.com>
2019-04-17 02:43:04 +0200

    Server - klasa startujaca logike serwera w mainie

---
+++ server-app/Server.cpp
@@ -0,0 +1,12 @@
+//
+// Created by zmus on 16/04/19.
+//
+
+#include "Server.h"
+
+void Server::start() {
+    connectionThread.initListeningSocket(server_addr); //nie jestem fanem tego rozwiazania, ale 2. wyjscie to trzymanie takiego samego server_addr w threadzie, a 3. to przekazywanie w 500 miejsc i cast na i potem z powtorem z void*
+    connectionThread.run();
+
+
+}
---
+++ server-app/Server.h
@@ -0,0 +1,31 @@
+//
+// Created by zmus on 16/04/19.
+//
+
+#ifndef TINYSPY_SERVER_H
+#define TINYSPY_SERVER_H
+
+
+#include <netinet/in.h>
+#include <functional>
+#include "Thread/ConnectionThread.h"
+
+class Server {
+private:
+    ConnectionThread connectionThread;
+    ConnectionCollector connectionCollector;
+    sockaddr_in server_addr;
+    int console_fd;
+public:
+    Server(int listening_port, int max_pend_conn) : connectionCollector(), connectionThread(std::ref(connectionCollector), console_fd, max_pend_conn) {
+        server_addr = {
+                .sin_family=AF_INET,
+                .sin_port=htons(listening_port)
+        };
+    }
+    void start();
+    int getListeningPort() { return ntohs(server_addr.sin_port); }
+};
+
+
+#endif //TINYSPY_SERVER_H
