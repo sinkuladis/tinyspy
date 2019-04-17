6ed399fd2b3ddd233f913cdc0b48448e2f5729bc
ziemus <musiatowiczz@gmail.com>
2019-04-17 13:12:18 +0200

    dalsza implementacja

--- server-app/Thread/ConnectionThread.cpp
+++ server-app/Thread/ConnectionThread.cpp
@@ -13,9 +13,6 @@
 #include<algorithm>
 #include <thread>
 
-typedef void * (ConnectionThread::*NonStaticMethodPtr)(void *);
-typedef void * (*FuncPtr)(void*);
-
 void ConnectionThread::run() {
     /*TODO nalezy odrzucic wywolanie, jesli thread juz dziala, z racji bezpieczenstwa
     moze byc kilka obiektow ConnectionThread, ale ta metoda moze byyc odpalona tylko raz, az skonczy sie ostatni thread - mutex trylock?*/
@@ -26,9 +23,8 @@ void* ConnectionThread::conn_routine(void* connectionThreadPtr) {
     ConnectionThread* ctptr = (ConnectionThread*)connectionThreadPtr; //TODO co jesli obiekt zostanie w tej chwili realokowany? -> musisz przekazac wskaznik na strukture, w ktorej polem bedzie referencja na obiekt
     ConnectionThread &connTrd = *ctptr;
 
-    fd_set exception_fds;
-    fd_set& listened_fds = std::ref(connTrd.listened_fds);
-    //int listenSock = connTrd.listenSock.getSockFd();
+    fd_set exception_fdset;
+    fd_set& listened_fdset = std::ref(connTrd.listened_fds);
 
     int nfds, max_fd;
 
@@ -39,11 +35,14 @@ void* ConnectionThread::conn_routine(void* connectionThreadPtr) {
             .tv_sec = 10,
             .tv_usec = 0
         };
-        FD_ZERO(&connTrd.listened_fds); FD_ZERO(&exception_fds);
+        FD_ZERO(&connTrd.listened_fds); FD_ZERO(&exception_fdset);
+        connTrd.connCollector.enter();
         max_fd = connTrd.initListenedFdSet();
-        exception_fds = listened_fds;
+        std::vector<int> conn_fds = connTrd.connCollector.getConnectionDescriptors();
+        connTrd.connCollector.leave(); // czy jednak po dodaniu?
+        exception_fdset = listened_fdset;
 
-        nfds = select(max_fd+1, &listened_fds, NULL, &exception_fds, &timeout);
+        nfds = select(max_fd+1, &listened_fdset, NULL, &exception_fdset, &timeout);
         if(nfds < 0){
             //TODO handle error
         }
@@ -51,20 +50,31 @@ void* ConnectionThread::conn_routine(void* connectionThreadPtr) {
             continue;
         else{
             //first check exception set on both, then read set on then, then ex on connections and at the end read on connection fds
-            if(FD_ISSET(connTrd.console_fd, &exception_fds)) {
+            if(FD_ISSET(connTrd.console_fd, &exception_fdset)) {
                 //TODO handle console exceptions
             }
-            if(FD_ISSET(connTrd.console_fd, &listened_fds)) {
+            if(FD_ISSET(connTrd.console_fd, &listened_fdset)) {
                 //TODO handle admin commands
             }
-            if(FD_ISSET(connTrd.listenSock.getSockFd(), &exception_fds)){
+            if(FD_ISSET(connTrd.listenSock.getSockFd(), &exception_fdset)){
                 //TODO handle listenSock exceptions
             }
-            if(FD_ISSET(connTrd.listenSock.getSockFd(), &listened_fds)) {
-                int new_sockfd = connTrd.listenSock.accept(); //jesli bedziemy zbierac w obiektach polaczen info o adresie, mozemy utworzyc sockaddr_in i przekazac jego adres
-                connTrd.connCollector.addConnection(new_sockfd);
+            if(FD_ISSET(connTrd.listenSock.getSockFd(), &listened_fdset)) {
+                connTrd.connCollector.enter();
+                connTrd.connCollector.addConnection(connTrd.listenSock);
+                connTrd.connCollector.leave();
             }
 
+            for (int fd : conn_fds) {
+                if (FD_ISSET(fd, &exception_fdset)) {
+                    //TODO handle connection exceptions
+                }
+                if(FD_ISSET(fd, &listened_fdset)) {
+                    connTrd.connCollector.readReceivedData(fd);
+                    //TODO daj znac wyzszej warstwie o tym, na jakim sockecie dostaniemy
+                }
+            }
+            //a moze powinienem tutaj dopiero wyjsc z collectora? w koncu bede operowal na polaczeniach, ktore moga zostac przerwane przez ten
 
         }
 
--- server-app/Thread/ConnectionThread.h
+++ server-app/Thread/ConnectionThread.h
@@ -5,28 +5,30 @@
 #ifndef TINYSPY_CONNECTIONTHREAD_H
 #define TINYSPY_CONNECTIONTHREAD_H
 
+#include <mutex>
+#include <functional>
 #include "../Connection/ConnectionCollector.h"
 #include "../Socket/Socket.h"
 
 class ConnectionThread {
 private:
-    ConnectionCollector &connCollector;
+    ConnectionCollector connCollector;
     Socket listenSock;
     fd_set listened_fds;
     int console_fd;
     int max_pending_conns;
     pthread_t thread_id;
+    std::mutex run_mutex;
     static void* conn_routine(void*);
     int initListenedFdSet();
 
 public:
     void initListeningSocket(sockaddr_in server_addr);
-    ConnectionThread(ConnectionCollector& nConnCollector, int n_console_fd, int n_max_connections)
-        : connCollector(nConnCollector), console_fd(n_console_fd), max_pending_conns(n_max_connections)
+    ConnectionThread(int n_console_fd, int n_max_connections)
+        : console_fd(n_console_fd), max_pending_conns(n_max_connections)
         {}
+        ConnectionCollector& getConnCollector() {return std::ref(connCollector);}
     void run();
-
-
 };
 
 
