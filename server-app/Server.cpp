cdf540f57fe1864bc3857b0dd87e938c6e1d1ca7
ziemus <musiatowiczz@gmail.com>
2019-04-19 18:42:36 +0200

    uruchomienie watku executorThread, utworzenie pipelineow miedzy threadami communicationThread i executorMainThread

--- server-app/Server.cpp
+++ server-app/Server.cpp
@@ -9,5 +9,8 @@ void Server::start() {
 
     connectionThread.initListeningSocket(server_addr); //nie jestem fanem tego rozwiazania, ale 2. wyjscie to trzymanie takiego samego server_addr w threadzie, a 3. to przekazywanie w 500 miejsc i cast na i potem z powtorem z void*
     connectionThread.run();
+    executorMainThread.run();
+
+    executorMainThread.join();
     connectionThread.join();
 }
--- server-app/Server.h
+++ server-app/Server.h
@@ -10,30 +10,32 @@
 #include <functional>
 #include "Thread/ConnectionThread.h"
 #include "Pipes/Pipe.h"
-#include "Execution/Executor.h"
+#include "Thread/ExecutorThread.h"
 
 class Server {
 private:
     Pipe consoleToConnectionPipe;
     //Pipe connectionToSerializerPipe;
     //Pipe serializerToExecutorPipe;
+    Pipe consoleToExecutorPipe;
     Pipe connectionToExecutorPipe;
     ConnectionThread connectionThread;
     ConnectionCollector connCollector;
 
     //Serializer serializer;
-    Executor executor;
+    ExecutorThread executorMainThread;
 
     sockaddr_in server_addr;
 public:
     Server(int listening_port, int max_pend_conn)
-        : consoleToConnectionPipe(),
-          //connectionToSerializerPipe(),
-          //serializerToExecutorPipe(),
-          connectionToExecutorPipe(),
+        : consoleToConnectionPipe(Pipe()),
+          //connectionToSerializerPipe(Pipe()),
+          //serializerToExecutorPipe(Pipe()),
+          connectionToExecutorPipe(Pipe()),
+          consoleToExecutorPipe(Pipe()),
           connCollector(),
-          connectionThread( std::ref(consoleToConnectionPipe), std::ref(connectionToExecutionPipe) , max_pend_conn, std::ref(connCollector)),
-          executor()
+          connectionThread( std::ref(consoleToConnectionPipe), std::ref(connectionToExecutorPipe) , max_pend_conn, std::ref(connCollector)),
+          executorMainThread(std::ref(connectionToExecutorPipe), std::ref(consoleToExecutorPipe), std::ref(connCollector))
     {
         server_addr = {
                 .sin_family=AF_INET,
