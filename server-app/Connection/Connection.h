ddd88b35c761357e1cd89101502073b63f145490
ziemus <musiatowiczz@gmail.com>
2019-04-20 03:32:48 +0200

    kolejkowanie i wstepna 'deserializacja' XD requestow

--- server-app/Connection/Connection.h
+++ server-app/Connection/Connection.h
@@ -7,23 +7,39 @@
 
 
 #include <iostream>
+#include <list>
 #include "../Socket/Socket.h"
+#include "../Request/Request.h"
 
 class Connection {
 protected:
     Socket sock;
     char in_buffer[1024];
     char out_buffer[1024];
-    //std::list<Task> taskQueue;
+    std::list<Request> requestQueue;
 public:
     Connection(Socket nSock);
     ~Connection() { sock.shut(); }
 
+    //TODO tutaj bedzie trzeba przeczytac
+    //1* rozmiar odebranej zserializowanej wiadomosci
+    //2* cala jej reszte
+    //obie rzeczy z uzyciem metody z klasy Socket, ktora zadba o przeczytanie zadanej liczby bajtow za kazdym razem
+    //tak, ze to odpalac bedziemy w ConnectionThread
+    //a nastepnie bedziemy dawac znac warstwie deserializujacej, ze zadane connection przyslalo dane do deserializacji
     void readReceivedData();
+    void mockAnswer();
+
     void writeDataToSend(char*);
-    void printMessage() {std::cout<<"Client "<<sock.getSockFd()<<" said "<< in_buffer<<std::endl;}
+    void printMessage() {std::cout<<"Client #"<<getId()<<" said "<< in_buffer<<std::endl;}
 
     Socket getSock() { return sock;}
+
+    //testowa metoda, tymczasowo id polaczenia to zwiazany z jego gniazdem file descriptor, unikalny dla polaczenia w trakcie jego dzialania
+    int getId() {return sock.getSockFd(); }
+
+    Request getNextRequest();
+
 };
 
 
