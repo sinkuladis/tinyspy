16ca0a58ed7962033192cf6f043be0f7c5bccc0f
ziemus <musiatowiczz@gmail.com>
2019-04-21 02:24:39 +0200

    obsługa konsoli

--- server-app/Server.cpp
+++ server-app/Server.cpp
@@ -5,12 +5,32 @@
 #include "Server.h"
 
 void Server::start() {
-
-
     connectionThread.initListeningSocket(server_addr); //nie jestem fanem tego rozwiazania, ale 2. wyjscie to trzymanie takiego samego server_addr w threadzie, a 3. to przekazywanie w 500 miejsc i cast na i potem z powtorem z void*
     connectionThread.run();
     executorMainThread.run();
 
-    executorMainThread.join();
+    consoleHandler.handleConsole();
+
     connectionThread.join();
+    executorMainThread.join();
 }
+
+int Server::getListeningPort() { return ntohs(server_addr.sin_port); }
+
+Server::Server(int listening_port, int max_pend_conn)
+        : consoleToConnectionPipe(Pipe()),
+        //connectionToSerializerPipe(Pipe()),
+        //serializerToExecutorPipe(Pipe()),
+          connectionToExecutorPipe(Pipe()),
+          consoleToExecutorPipe(Pipe()),
+          connCollector(),
+          consoleHandler(std::ref(consoleToConnectionPipe), std::ref(consoleToExecutorPipe)),
+          connectionThread( std::ref(consoleToConnectionPipe), std::ref(connectionToExecutorPipe) , max_pend_conn, std::ref(connCollector)),
+          executorMainThread(std::ref(connectionToExecutorPipe), std::ref(consoleToExecutorPipe), std::ref(connCollector))
+{
+    server_addr = {
+            .sin_family=AF_INET,
+            .sin_port=htons(listening_port)
+    };
+}
+
--- server-app/Server.h
+++ server-app/Server.h
@@ -11,6 +11,7 @@
 #include "Thread/ConnectionThread.h"
 #include "Pipes/Pipe.h"
 #include "Thread/ExecutorThread.h"
+#include "Console/ConsoleHandler.h"
 
 class Server {
 private:
@@ -21,29 +22,15 @@ private:
     Pipe connectionToExecutorPipe;
     ConnectionThread connectionThread;
     ConnectionCollector connCollector;
+    ConsoleHandler consoleHandler;
 
-    //Serializer serializer;
     ExecutorThread executorMainThread;
 
     sockaddr_in server_addr;
 public:
-    Server(int listening_port, int max_pend_conn)
-        : consoleToConnectionPipe(Pipe()),
-          //connectionToSerializerPipe(Pipe()),
-          //serializerToExecutorPipe(Pipe()),
-          connectionToExecutorPipe(Pipe()),
-          consoleToExecutorPipe(Pipe()),
-          connCollector(),
-          connectionThread( std::ref(consoleToConnectionPipe), std::ref(connectionToExecutorPipe) , max_pend_conn, std::ref(connCollector)),
-          executorMainThread(std::ref(connectionToExecutorPipe), std::ref(consoleToExecutorPipe), std::ref(connCollector))
-    {
-        server_addr = {
-                .sin_family=AF_INET,
-                .sin_port=htons(listening_port)
-        };
-    }
+    Server(int listening_port, int max_pend_conn);
     void start();
-    int getListeningPort() { return ntohs(server_addr.sin_port); }
+    int getListeningPort();
 };
 
 
